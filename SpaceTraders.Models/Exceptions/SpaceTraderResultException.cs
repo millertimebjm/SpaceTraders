ab4883d965b2a84a3702a936d0e54544aef62373
Brandon Miller <millertimebjm@hotmail.com>
2026-03-19 11:48:50 -0500

    Fix exception

--- SpaceTraders.Models/Exceptions/SpaceTraderResultException.cs
+++ SpaceTraders.Models/Exceptions/SpaceTraderResultException.cs
@@ -2,10 +2,16 @@ using SpaceTraders.Models;
 
 namespace SpaceTraders.Model.Exceptions;
 
+// This is the Primary Constructor
 public class SpaceTraderResultException(
     string Message,
-    HttpRequestException Exception,
-     string responseBody) : Exception(Message, Exception)
+    HttpRequestException? Exception,
+    string responseBody) : Exception(Message, Exception)
 {
+    public SpaceTraderResultException(string? message) 
+        : this(message ?? string.Empty, null, string.Empty)
+    {
+    }
+
     public string ResponseBody { get; } = responseBody;
 }
