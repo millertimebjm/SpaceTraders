f4de1487c5d1f7f75df959ba1140c34c3bb5cbc9
Brandon Miller <millertimebjm@hotmail.com>
2026-04-14 17:05:20 -0500

    Add Ships Waiting to homepage

--- SpaceTraders.Mvc/Controllers/HomeController.cs
+++ SpaceTraders.Mvc/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SpaceTraders.Mvc.Models;
 using SpaceTraders.Services.Agents.Interfaces;
+using SpaceTraders.Services.Ships;
 using SpaceTraders.Services.ShipStatuses.Interfaces;
 using SpaceTraders.Services.Systems.Interfaces;
 
@@ -14,6 +15,12 @@ public class HomeController(
 ) : BaseController(_agentsService, _shipStatusesCacheService, _systemsService)
 {
     public async Task<IActionResult> Index()
+    {
+        var shipStatuses = await _shipStatusesCacheService.GetAsync();
+        return View(shipStatuses);
+    }
+
+    public async Task<IActionResult> ShipsWaiting()
     {
         var shipStatuses = await _shipStatusesCacheService.GetAsync();
         return View(shipStatuses);
